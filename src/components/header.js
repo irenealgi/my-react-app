a633542bcca877bd7e15895d4488b5dd794bfca3
Irene Alegre <irenealgi@gmail.com>
2019-03-08 17:37:43 +0000

    Lessons 4, 5 , 6, 7

--- src/components/header.js
+++ src/components/header.js
@@ -3,16 +3,46 @@ import Link from 'gatsby-link'
 import logo from '../images/logo-designcode.svg';
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/"><img src={logo} width="30" /></Link>
-      <Link to="/work">Work</Link>
-      <Link to="/blog">Blog</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact"><button>Contact</button></Link>
+
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll',
+  this.handleScroll)
+}
+
+handleScroll = (event) => {
+  const scrollTop = window.pageYOffset
+
+  if (scrollTop > 50) {
+    this.setState({ hasScrolled: true })
+  } else {
+    this.setState({ hasScrolled: false })
+  }
+}
+
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src={logo} width="30" /></Link>
+          <Link to="/work">Courses</Link>
+          <Link to="/blog">Resources</Link>
+          <Link to="/about">Pricing</Link>
+          <Link to="/contact"><button>Get access</button></Link>
+          </div>
       </div>
-  </div>
-)
+    )
+  }
+}
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,46 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Card from '../components/Card';
+import Section from '../components/Section';
+import Wave from '../components/Wave';
+
 
 const IndexPage = () => (
-<div>
   <div>
-    <div className="Hero">
-      <div className="HeroGroup">
-        <h1>Irene Alegre</h1>
-        <p>Product Designer based in London</p>
-        <Link to="/page-2/">See projects</Link>
+    <div>
+      <div className="Hero">
+        <div className="HeroGroup">
+          <h1>Learn your English</h1>
+          <p>Resources, lessons and face to face learning</p>
+          <Link to="/page-2/">See courses</Link>
+        </div>
+        <Wave />
       </div>
     </div>
+    <div className="Cards">
+      <h2>3 courses, more coming</h2>
+      <div className="CardGroup">
+        <Card
+          title="English for travelling"
+          text="4 weeks"
+          image={require('../images/travel.png')} />
+        <Card
+          title="English for life"
+          text="6 months"
+          image={require('../images/relationships.png')} />
+        <Card
+          title="English for business"
+          text="3 months"
+          image={require('../images/business.png')} />
+      </div>
+    </div>
+    <Section
+      image={require('../images/bookshelf.jpg')}
+      logo={require('../images/logo-react.png')}
+      title="English lessons for everyone"
+      text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+    />
   </div>
-</div>
 )
 
 export default IndexPage
